84725eb618d003a800b637dfd1a12bbe41428b75
LiveIsBigStange <98121339+LiveIsBigStange@users.noreply.github.com>
2023-01-25 16:38:18 +0300

    Testing autocomliter

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { PriceListService } from 'src/app/services/pricelist.service';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 
 @Component({
@@ -10,20 +12,34 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
   styleUrls: ['./product-detail.component.css'],
   providers: [PriceListService]
 })
-export class ProductDetailComponent  {
+export class ProductDetailComponent {
+
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]> = new Observable();
 
   form: FormGroup = new FormGroup({
     price: new FormControl(),
     name: new FormControl(),
     description: new FormControl(),
-    productCode:  new FormControl()
-    
+    productCode: new FormControl()
+
   });
 
-  
+
   constructor(private priceListService: PriceListService, private router: Router, private fb: FormBuilder) { }
 
-  
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
 
   save(form: any): void {
     this.priceListService.savePriceList({ price: this.form.value['price'], name: this.form.value['name'], description: this.form.value['description'], productCode: this.form.value['productCode'] }).subscribe(
